249adf4fccd48d65bfaaa4e51196ccf81a41f5cc
Ariel Slonimsqui <arislonimsqui@gmail.com>
2025-04-15 16:54:06 -0300

    Primera Version Funcional

--- src/TP1.hs
+++ src/TP1.hs
@@ -1,14 +1,23 @@
 module TP1 where
 import PdePreludat
+import Test.Hspec (xcontext)
 
 esMes :: Number -> Bool
-esMes = undefined
+esMes x = x >= 1 && x <= 12
 
 mesAnterior :: Number -> Number
-mesAnterior = undefined
+mesAnterior 1 = 12
+mesAnterior x | esMes(x) = x-1 --Utiliza esMes para comprobar que estemos dentro del dominio del problema
 
 mesSiguiente :: Number -> Number
-mesSiguiente = undefined
+mesSiguiente 12 = 1
+mesSiguiente x | esMes(x) = x+1 --Utiliza esMes para comprobar que estemos dentro del dominio del problema.
 
 estacion :: Number -> String
-estacion = undefined
+estacion 1 = "verano"
+estacion 4 = "otonio"
+estacion 7 = "invierno"
+estacion 10 = "primavera"
+estacion x
+     | mod x 3 == 0 = (estacion (mesAnterior x)) ++ "/" ++ (estacion (mesSiguiente x))
+     | otherwise = (estacion (mesAnterior x))  --En caso de ingresar un numero por fuera del dominio, va a saltar el error cuando se llame a mesAnterior, por lo que no hace falta preguntar por si esMes(x) es True.
