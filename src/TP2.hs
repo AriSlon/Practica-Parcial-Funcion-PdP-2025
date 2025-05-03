e1a61f7e108bd6351074faf5549dcc140825932e
Ariel Slonimsqui <arislonimsqui@gmail.com>
2025-05-03 12:18:56 -0300

    Cambios que favorecen la expresividad

--- src/TP2.hs
+++ src/TP2.hs
@@ -1,22 +1,14 @@
 module TP2 where
 import PdePreludat
 
-type Hashtag = String 
-
-luisito :: Video
-luisito = Video{
-  titulo = "mexico",
-  minutos = 90,
-  hashtags = ["hola", "chau"]
-} 
-
+type Hashtag = String --Expresividad
+type Minutos = Number --Expresividad
 
 data Video = Video {
   titulo :: String,
   minutos :: Number,
   hashtags :: [String]
 } deriving (Show, Eq)
-
 
 
 data Playlist = Playlist {
@@ -28,19 +20,17 @@ data Playlist = Playlist {
 tieneHashtag :: Hashtag -> Video -> Bool
 tieneHashtag hashtag video = elem hashtag (hashtags video)
 
-getMinutos:: [Video] -> [Number]
-getMinutos  =  map minutos --Ap Parcial ([Video])
+getMinutos:: [Video] -> [Minutos]
+getMinutos  =  map minutos --Point-free ([Video])
 
 getVideosFiltrados:: Hashtag -> [Video] -> [Video]
-getVideosFiltrados hashtag  = filter (tieneHashtag hashtag) --Ap Parcial ([Video])
+getVideosFiltrados hashtag  = filter (tieneHashtag hashtag) --Point-free ([Video])
 
-
-minutosTotalesConHashtag :: Hashtag -> Playlist -> Number
-minutosTotalesConHashtag hashtag  = sum . getMinutos . getVideosFiltrados hashtag . videos 
-
+minutosTotalesConHashtag :: Hashtag -> Playlist -> Minutos
+minutosTotalesConHashtag hashtag  = sum . getMinutos . getVideosFiltrados hashtag . videos --getMinutos y getVideosFiltrados no son necesarias, pero considero que favorecen la expresividad.
 
 estanRelacionados :: Video -> Video -> Bool
-estanRelacionados video1 video2 = any (`tieneHashtag` video2)(hashtags video1)
+estanRelacionados video1 video2 = any (`tieneHashtag` video1) (hashtags video2) --tieneHashtag esta entre comillas invertidas con el objetivo de fijar el segundo parametro. Ahora, "`tieneHashtag` video2" es una funcion que recibe un Hashtag y devuelve un Bool.
 
 recomendable :: Video -> Playlist -> Bool
-recomendable video playlist = (>= 2) (length (filter (estanRelacionados video) (videos playlist)))
+recomendable video = (>= 2) . length . filter (estanRelacionados video) . videos --En este caso es necesaria la notacion point-free para poder componer todas las funciones. Si explicito el parametro "playlist", "videos playlist" ya no seria una funcion, sino un valor concreto (lista de videos).
