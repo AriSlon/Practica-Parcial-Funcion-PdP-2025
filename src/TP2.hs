7bd79eeed1ee4ccace64549cf6b0ddc6cca9e129
Ariel Slonimsqui <arislonimsqui@gmail.com>
2025-05-03 12:10:46 -0300

    Desarrollo funciones estanRelacionados y recomendable

--- src/TP2.hs
+++ src/TP2.hs
@@ -1,25 +1,46 @@
 module TP2 where
 import PdePreludat
 
+type Hashtag = String 
+
+luisito :: Video
+luisito = Video{
+  titulo = "mexico",
+  minutos = 90,
+  hashtags = ["hola", "chau"]
+} 
+
+
 data Video = Video {
   titulo :: String,
   minutos :: Number,
   hashtags :: [String]
 } deriving (Show, Eq)
 
+
+
 data Playlist = Playlist {
   descripcion :: String,
   videos :: [Video]
 } deriving (Show, Eq)
 
-tieneHashtag :: String -> Video -> Bool
-tieneHashtag = undefined
 
-minutosTotalesConHashtag :: String -> Playlist -> Number
-minutosTotalesConHashtag = undefined
+tieneHashtag :: Hashtag -> Video -> Bool
+tieneHashtag hashtag video = elem hashtag (hashtags video)
+
+getMinutos:: [Video] -> [Number]
+getMinutos  =  map minutos --Ap Parcial ([Video])
+
+getVideosFiltrados:: Hashtag -> [Video] -> [Video]
+getVideosFiltrados hashtag  = filter (tieneHashtag hashtag) --Ap Parcial ([Video])
+
+
+minutosTotalesConHashtag :: Hashtag -> Playlist -> Number
+minutosTotalesConHashtag hashtag  = sum . getMinutos . getVideosFiltrados hashtag . videos 
+
 
 estanRelacionados :: Video -> Video -> Bool
-estanRelacionados = undefined
+estanRelacionados video1 video2 = any (`tieneHashtag` video2)(hashtags video1)
 
 recomendable :: Video -> Playlist -> Bool
-recomendable = undefined
+recomendable video playlist = (>= 2) (length (filter (estanRelacionados video) (videos playlist)))
